32ddd98a86fb001a118c36ce3ae53d6eff771e7e
Vivek <ojhavivek24@gmail.com>
2025-10-08 23:50:15 +0530

    added theme section

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,12 +1,14 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import Theme from "./Theme";
 // import Footer from "./Footer";
 
 const Landing = () => {
   return (
-    <div className="flex flex-col h-999 bg-gray-100">
+    <div className="w-full max-w-7xl mx-auto flex flex-col">
       <Navbar />
       <Hero />
+      <Theme />
       {/* <Footer /> */}
     </div>
   );
