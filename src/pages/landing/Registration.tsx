7b456cb410e5e453c2d9daf69dce8c38f65c32b5
VinayyOjha <ojhavinay7602@.com>
2025-10-08 23:55:41 +0530

    registration fee

--- src/pages/landing/Registration.tsx
+++ src/pages/landing/Registration.tsx
@@ -1,7 +1,7 @@
 const RegistrationFee = () => {
     return (
         <section id="registration" className="mt-30 p-4 min-w-5xl mx-auto rounded-md justify-center bg-white/70">
-            <div className="w-full font-bold text-3xl text-center">
+            <div className=" font-bold text-3xl text-center">
                 Registration Fee
             </div>
             <div className="flex flex-col m-12">
@@ -34,6 +34,16 @@ const RegistrationFee = () => {
                     </div>
                 </div>
 
+            </div>
+
+            {/* Register and payment link */}
+            <div className="text-white flex flex-co\l items-center justify-center text-center gap-4">
+                <div className="px-3 py-2 w-40 rounded-sm bg-blue-600">
+                    Registration Link
+                </div>
+                <div className="px-3 py-2 w-40 rounded-sm bg-blue-600">
+                    Click Here To Pay
+                </div>
             </div>
         </section>
     )
