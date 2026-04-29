a436e7393aaf21c28d1076df72f93d39c9a79b13
Vivek <ojhavivek24@gmail.com>
2026-04-29 09:04:30 +0530

    updated schedule dates

--- src/pages/landing/Schedule.tsx
+++ src/pages/landing/Schedule.tsx
@@ -8,11 +8,11 @@ const Schedule = () => {
       <div className="px-2 w-full max-w-6xl flex flex-col gap-2">
         <div className="w-full p-2 flex bg-neutral-200 rounded-lg">
           <p className="flex-1 text-xl font-semibold">Final Paper Submission</p>
-          <p className="flex-1 text-lg text-center">May 1, 2026</p>
+          <p className="flex-1 text-lg text-center"><s>1 May, 2026</s>&nbsp; 20<sup>th</sup> May, 2026</p>
         </div>
         <div className="w-full px-2 flex">
           <p className="flex-1 text-xl font-semibold">Registration (Last Date)</p>
-          <p className="flex-1 text-lg text-center">May 15, 2026</p>
+          <p className="flex-1 text-lg text-center"><s>15 May, 2026</s>&nbsp; 5<sup>th</sup> June, 2026</p>
         </div>
       </div>
     </div>
